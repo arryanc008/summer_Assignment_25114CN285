5fbf079d8da0a617b575c558a8b2ee332e33fa85
arryanc008 <aryanchourasia0806@gmail.com>
2026-06-13 21:49:38 +0530

    Add files via upload

---
+++ Day_09.java
@@ -0,0 +1,68 @@
+// Write a program to Print reverse star pattern......
+public class Day_09.java {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+
+// Write a program to Print reverse number triangle......
+public class Day_09.java {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+// Write a program to Print repeated character pattern............
+public class Day_09.java {
+    public static void main(String[] args) {
+        char ch = 'A';
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch);
+            }
+            ch++;
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+
+
+// Write a program to Print hollow square pattern.............
+public class Day_09.java {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
